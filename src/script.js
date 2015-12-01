e6d46fab9361e2d8d527f9d31e020f5b39f1b207
GrimmOutlook <grimmoutlook87@gmail.com>
2015-11-30 22:19:34 -0500

    Used React Properties to render list of items.

--- src/script.js
+++ src/script.js
@@ -3,31 +3,29 @@ var Todo = React.createClass({
   render: function(){
 
     return(
-      <div>
-
-        <h1>To Do List:</h1>
-        <div className="form-inline">
-            <div className="form-group">
-              <input className="form-control" placeholder="Add Task" />
-              <button className='btn btn-default btn sm'>+</button>
-            </div>
-          </div>
 
         <ul>
-          <li className='todo'>Laundry</li>
-          <li className='todo'>Vacuum</li>
-
-
+          <li className='todo'>{this.props.todo}</li>
         </ul>
 
-
-
-
-      </div>
       );
 
   }
 
 });
 
-React.render(<Todo />, document.getElementById('todo'));
+React.render(<div>
+
+              <h1>To Do List:</h1>
+                <div className="form-inline">
+                  <div className="form-group">
+                    <input className="form-control" placeholder="Add Task" />
+                    <button className='btn btn-default btn sm'>+</button>
+                  </div>
+                </div>
+
+              <Todo todo="Letter to Enterprise" />
+              <Todo todo="$ to Aaron Tuesday" />
+              <Todo todo="Contact Cynthia - re: Turkey" />
+              <Todo todo="Much Coding" />
+            </div>, document.getElementById('todo'));
